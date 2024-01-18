807b0147bca8292a3bd98bc1fbfc6189ef0db8ab
Omer Leibo <leibo.omer13@gmail.com>
2024-01-18 15:00:51 +0200

    delete db

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+# from flask_sqlalchemy import SQLAlchemy
 from flask_socketio import SocketIO
 from flask_cors import CORS
 
@@ -7,14 +7,14 @@ HOST = "localhost"
 
 app = Flask(__name__)
 CORS(app)
-app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql://postgres:postgres@{HOST}:5432/postgres"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-db = SQLAlchemy(app)
+# app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql://postgres:postgres@{HOST}:5432/postgres"
+# app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+# db = SQLAlchemy(app)
 socketio = SocketIO(app, cors_allowed_origins="*")
 
 from app import routes
 
 
 
-with app.app_context():
-    db.create_all()
+# with app.app_context():
+#     db.create_all()
